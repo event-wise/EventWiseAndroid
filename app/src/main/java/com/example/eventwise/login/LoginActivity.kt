2eeb727b0dfdc81c42f7a40ceea698ea8b160cdb
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-11-06 15:32:14 +0100

    LoginScreen: generate layout file

---
+++ app/src/main/java/com/example/eventwise/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+
+package com.example.eventwise.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eventwise.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
