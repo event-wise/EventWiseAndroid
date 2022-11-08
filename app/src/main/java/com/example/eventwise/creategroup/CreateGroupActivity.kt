c329ec33b364035aa1753b93c4aafa9457f1f12e
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-11-08 19:17:30 +0100

    CreateGroupScreen: generate basic layout

---
+++ app/src/main/java/com/example/eventwise/creategroup/CreateGroupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eventwise.creategroup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eventwise.R
+
+class CreateGroupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_group)
+    }
+}
