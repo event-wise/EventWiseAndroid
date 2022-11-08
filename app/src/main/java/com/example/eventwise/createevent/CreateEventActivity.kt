57193396fe42f71b2ac66f6a803b8692c338f12c
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-11-08 19:51:16 +0100

    CreateEventScreen: generate basic layout file

---
+++ app/src/main/java/com/example/eventwise/createevent/CreateEventActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eventwise.createevent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eventwise.R
+
+class CreateEventActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_event)
+    }
+}
