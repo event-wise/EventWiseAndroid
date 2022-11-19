193e08f879eb889b36e8de0610b388cf0e3431b9
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-11-19 18:42:33 +0100

    UpdateEventScreen: add event type

--- app/src/main/java/com/example/eventwise/updateevent/UpdateEventActivity.kt
+++ app/src/main/java/com/example/eventwise/updateevent/UpdateEventActivity.kt
@@ -2,11 +2,35 @@ package com.example.eventwise.updateevent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.TextView
 import com.example.eventwise.R
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class UpdateEventActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_event)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.event_types,
+            android.R.layout.simple_spinner_dropdown_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            val dropdown = findViewById<MaterialAutoCompleteTextView>(R.id.updateEventActivityEditTextEventType)
+            dropdown.setAdapter(adapter)
+
+            val dropdownLayout = findViewById<TextInputLayout>(R.id.updateEventActivityEditTextEventTypeLayout)
+            dropdownLayout.setOnClickListener {
+                dropdown.showDropDown()
+            }
+            dropdown.setOnClickListener {
+                dropdown.showDropDown()
+            }
+        }
     }
 }
