bae4392bb43a65bdac2244268506a8de32799639
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-11-19 19:13:22 +0100

    CreateEventScreen: add event type

--- app/src/main/java/com/example/eventwise/createevent/CreateEventActivity.kt
+++ app/src/main/java/com/example/eventwise/createevent/CreateEventActivity.kt
@@ -2,11 +2,32 @@ package com.example.eventwise.createevent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.eventwise.R
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.google.android.material.textfield.TextInputLayout
 
 class CreateEventActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_event)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.event_types,
+            android.R.layout.simple_spinner_dropdown_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            val dropdown = findViewById<MaterialAutoCompleteTextView>(R.id.createEventActivityEditTextEventType)
+            dropdown.setAdapter(adapter)
+
+            val dropdownLayout = findViewById<TextInputLayout>(R.id.createEventActivityEditTextEventTypeLayout)
+            dropdownLayout.setOnClickListener {
+                dropdown.showDropDown()
+            }
+            dropdown.setOnClickListener {
+                dropdown.showDropDown()
+            }
+        }
     }
 }
