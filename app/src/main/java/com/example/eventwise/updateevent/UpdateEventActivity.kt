fba871fad6724a2adfd6d0b60b11e179c37d9319
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-11-19 13:17:14 +0100

    UpdateEventScreen: generate basic layout

---
+++ app/src/main/java/com/example/eventwise/updateevent/UpdateEventActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eventwise.updateevent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eventwise.R
+
+class UpdateEventActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_event)
+    }
+}
