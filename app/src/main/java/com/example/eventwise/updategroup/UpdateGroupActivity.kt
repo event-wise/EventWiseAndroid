79f8801ff14780d53e235ea26b85b912943d13b3
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-11-19 12:29:44 +0100

    UpdateGroupScreen: generate basic layout

---
+++ app/src/main/java/com/example/eventwise/updategroup/UpdateGroupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eventwise.updategroup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eventwise.R
+
+class UpdateGroupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_group)
+    }
+}
