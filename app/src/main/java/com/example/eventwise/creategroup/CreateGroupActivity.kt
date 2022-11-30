1bfb944613e533fcfcda848b0d81d1cfb12bf68f
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-11-30 19:42:07 +0100

    CreateGroupScreen: create data binding

--- app/src/main/java/com/example/eventwise/creategroup/CreateGroupActivity.kt
+++ app/src/main/java/com/example/eventwise/creategroup/CreateGroupActivity.kt
@@ -2,11 +2,20 @@ package com.example.eventwise.creategroup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.eventwise.R
+import com.example.eventwise.databinding.ActivityCreateGroupBinding
 
 class CreateGroupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreateGroupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_group)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_create_group)
+
+        binding.lifecycleOwner = this
+
+        binding.viewModel = CreateGroupViewModel()
     }
 }
