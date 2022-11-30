af9d341b95769cb690897818a8c494d17f7c6618
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-11-30 20:06:37 +0100

    ChangePasswordScreen: create data binding

--- app/src/main/java/com/example/eventwise/changepassword/ChangePasswordActivity.kt
+++ app/src/main/java/com/example/eventwise/changepassword/ChangePasswordActivity.kt
@@ -2,11 +2,23 @@ package com.example.eventwise.changepassword
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.eventwise.R
+import com.example.eventwise.databinding.ActivityChangePasswordBinding
 
 class ChangePasswordActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChangePasswordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_change_password)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_change_password)
+
+        binding.lifecycleOwner = this
+
+        binding.viewModel = ChangePasswordViewModel()
+
+
     }
 }
