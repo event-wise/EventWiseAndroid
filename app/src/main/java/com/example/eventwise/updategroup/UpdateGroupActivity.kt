46a966c5a025080ddabdc9a43b22e107410877a3
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-11-30 19:47:39 +0100

    UpdateGroupScreen: create data binding

--- app/src/main/java/com/example/eventwise/updategroup/UpdateGroupActivity.kt
+++ app/src/main/java/com/example/eventwise/updategroup/UpdateGroupActivity.kt
@@ -2,11 +2,20 @@ package com.example.eventwise.updategroup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.eventwise.R
+import com.example.eventwise.databinding.ActivityUpdateGroupBinding
 
 class UpdateGroupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUpdateGroupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_update_group)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_update_group)
+
+        binding.lifecycleOwner = this
+
+        binding.viewModel = UpdateGroupViewModel()
     }
 }
