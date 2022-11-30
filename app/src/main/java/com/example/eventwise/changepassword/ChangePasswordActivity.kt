36c6bf5e42a94453718bcf77d98cffa4d3c6284a
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-11-30 20:01:51 +0100

    ChangePasswordScreen: generate basic layout

---
+++ app/src/main/java/com/example/eventwise/changepassword/ChangePasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eventwise.changepassword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eventwise.R
+
+class ChangePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+    }
+}
