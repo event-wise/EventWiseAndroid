1e238b2384b266e7444b25261b479507a2106268
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-11-30 19:04:03 +0100

    SignUpScreen: create data binding

--- app/src/main/java/com/example/eventwise/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/eventwise/signup/SignUpActivity.kt
@@ -2,11 +2,24 @@ package com.example.eventwise.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.eventwise.R
+import com.example.eventwise.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+
+        binding.lifecycleOwner = this
+
+        binding.viewModel = SignUpViewModel()
+
+
+
     }
 }
