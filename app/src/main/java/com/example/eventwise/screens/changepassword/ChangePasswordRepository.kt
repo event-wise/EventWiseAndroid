6fecdfff214b1b02e600669dbae21a439f4123e1
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-12-03 21:51:48 +0100

    ChangePasswordScreen: add repository and viewmodel functions

--- app/src/main/java/com/example/eventwise/screens/changepassword/ChangePasswordRepository.kt
+++ app/src/main/java/com/example/eventwise/screens/changepassword/ChangePasswordRepository.kt
@@ -1,4 +1,22 @@
 package com.example.eventwise.screens.changepassword
 
+import com.example.eventwise.models.PasswordChangeRequestModel
+import com.example.eventwise.services.GatewayApi
+
 class ChangePasswordRepository {
+
+    suspend fun changePassword(
+        currentPassword: String,
+        newPassword: String,
+        newPasswordConfirmation: String
+    ) {
+        GatewayApi.gatewayService.changePassword(
+            PasswordChangeRequestModel(
+                currentPassword,
+                newPassword,
+                newPasswordConfirmation
+            )
+        )
+    }
+
 }
