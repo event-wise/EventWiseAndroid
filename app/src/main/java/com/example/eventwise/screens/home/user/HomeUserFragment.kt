1c464c8e8803dfc5664ff56f80c818adcf57400a
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-12-03 18:29:25 +0100

    HomeScreen: add click listeners to buttons in User tab

--- app/src/main/java/com/example/eventwise/screens/home/user/HomeUserFragment.kt
+++ app/src/main/java/com/example/eventwise/screens/home/user/HomeUserFragment.kt
@@ -1,5 +1,6 @@
 package com.example.eventwise.screens.home.user
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +10,9 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import com.example.eventwise.R
 import com.example.eventwise.databinding.FragmentHomeUserBinding
+import com.example.eventwise.screens.changepassword.ChangePasswordActivity
+import com.example.eventwise.screens.login.LoginActivity
+import com.example.eventwise.services.Constants
 
 
 class HomeUserFragment : Fragment() {
@@ -27,6 +31,27 @@ class HomeUserFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
 
         binding.viewModel = viewModel
+
+        binding.createEventActivityChangePasswordButton.setOnClickListener {
+            startActivity(Intent(requireActivity(), ChangePasswordActivity::class.java))
+        }
+
+        binding.createEventActivityLogOutButton.setOnClickListener {
+            requireActivity().finish()
+            startActivity(Intent(requireActivity(), LoginActivity::class.java))
+        }
+
+        binding.createEventActivitySaveButton.setOnClickListener {
+            viewModel.updateProfileInformation()
+        }
+
+        binding.createEventActivityLogOutButton.setOnClickListener {
+            viewModel.logOut()
+            Constants.BEARER_TOKEN = "Bearer token"
+            Constants.GLOBAL_USER_ID = -1
+            startActivity(Intent(requireActivity(), LoginActivity::class.java))
+            requireActivity().finish()
+        }
 
         return binding.root
     }
