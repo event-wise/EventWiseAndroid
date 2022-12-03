a49b23c7f95c31d99f96159d7ec3a4360cd6e0ad
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-12-03 22:00:45 +0100

    LoginScreen: add repository and viewmodel functions

--- app/src/main/java/com/example/eventwise/screens/login/LoginActivity.kt
+++ app/src/main/java/com/example/eventwise/screens/login/LoginActivity.kt
@@ -4,6 +4,7 @@ package com.example.eventwise.screens.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.example.eventwise.R
 import com.example.eventwise.databinding.ActivityLoginBinding
@@ -14,6 +15,8 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
+    private val loginActivityViewModel: LoginActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,7 +24,7 @@ class LoginActivity : AppCompatActivity() {
 
         binding.lifecycleOwner = this
 
-        binding.viewModel = LoginActivityViewModel()
+        binding.viewModel = loginActivityViewModel
 
         binding.loginActivitySignUpButton.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
