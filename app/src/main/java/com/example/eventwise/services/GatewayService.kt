a1f3d74a11e367796d81bf7b9494b8d7a7f88abc
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-12-03 17:23:33 +0100

    Service: add missing authorization headers

--- app/src/main/java/com/example/eventwise/services/GatewayService.kt
+++ app/src/main/java/com/example/eventwise/services/GatewayService.kt
@@ -16,22 +16,20 @@ import com.example.eventwise.models.GroupDetailsModel
 import com.example.eventwise.models.GroupsModel
 import com.example.eventwise.models.SearchResponseModel
 import retrofit2.Response
-import retrofit2.http.POST
-import retrofit2.http.GET
-import retrofit2.http.Body
-import retrofit2.http.DELETE
-import retrofit2.http.PUT
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface GatewayService {
 
     @POST("api/account/change-password")
     suspend fun changePassword(
-        @Body passwordChangeRequestModel: PasswordChangeRequestModel
+        @Body passwordChangeRequestModel: PasswordChangeRequestModel,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<ResponseModel>
 
     @DELETE("api/account/delete-account")
-    suspend fun deleteAccount(): Response<ResponseModel>
+    suspend fun deleteAccount(
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
+    ): Response<ResponseModel>
 
     @POST("api/account/login")
     suspend fun authenticateUser(
@@ -39,105 +37,125 @@ interface GatewayService {
     ): Response<LoginResponseModel>
 
     @GET("api/account/logout")
-    suspend fun logout(): Response<ResponseModel>
+    suspend fun logout(
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
+    ): Response<ResponseModel>
 
     @GET("api/account/profile")
     suspend fun profile(
-        @Query("id") id: Long
+        @Query("id") id: Long,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<UserModel>
 
     @POST("api/account/register")
     suspend fun register(
-        @Body registerRequestModel: RegisterRequestModel
+        @Body registerRequestModel: RegisterRequestModel,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<ResponseModel>
 
     @POST("api/account/update-profile")
     suspend fun updateProfile(
-        @Body profileUpdateRequestModel: ProfileUpdateRequestModel
+        @Body profileUpdateRequestModel: ProfileUpdateRequestModel,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<ResponseModel>
 
     @PUT("api/event/accept-event")
     suspend fun acceptEvent(
         @Query("eventId") eventId: Long,
-        @Query("userId") userId: Long
+        @Query("userId") userId: Long,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<ResponseModel>
 
     @POST("api/event/create-event")
     suspend fun createEvent(
-        @Body eventSaveModel: EventSaveModel
+        @Body eventSaveModel: EventSaveModel,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<ResponseModel>
 
     @DELETE("api/event/delete-event")
     suspend fun deleteEvent(
         @Query("eventId") eventId: Long,
-        @Query("userId") userId: Long
+        @Query("userId") userId: Long,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<ResponseModel>
 
     @GET("api/event/event-details")
     suspend fun getEventDetails(
         @Query("eventId") eventId: Long,
-        @Query("userId") userId: Long
+        @Query("userId") userId: Long,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<EventDetailsModel>
 
     @GET("api/event/list-user-events")
     suspend fun listUserEvents(
-        @Query("userId") userId: Long
+        @Query("userId") userId: Long,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<List<EventsModel>>
 
     @PUT("api/event/reject-event")
     suspend fun rejectEvent(
         @Query("eventId") eventId: Long,
-        @Query("userId") userId: Long
+        @Query("userId") userId: Long,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<ResponseModel>
 
     @POST("api/event/update-event")
     suspend fun updateEvent(
-        @Body eventSaveModel: EventSaveModel
+        @Body eventSaveModel: EventSaveModel,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<ResponseModel>
 
     @POST("api/group/add-remove-member")
     suspend fun addRemoveMember(
-        @Body memberAddRemoveModel: MemberAddRemoveModel
+        @Body memberAddRemoveModel: MemberAddRemoveModel,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<ResponseModel>
 
     @POST("api/group/create-group")
     suspend fun createGroup(
-        @Body groupSaveModel: GroupSaveModel
+        @Body groupSaveModel: GroupSaveModel,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<ResponseModel>
 
     @DELETE("api/group/delete-group")
     suspend fun deleteGroup(
         @Query("groupId") groupId: Long,
-        @Query("userId") userId: Long
+        @Query("userId") userId: Long,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<ResponseModel>
 
     @PUT("api/group/exit-from-group")
     suspend fun exitFromGroup(
         @Query("groupId") groupId: Long,
-        @Query("userId") userId: Long
+        @Query("userId") userId: Long,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<ResponseModel>
 
     @GET("api/group/get-group-details")
     suspend fun getGroupDetails(
         @Query("groupId") groupId: Long,
-        @Query("userId") userId: Long
+        @Query("userId") userId: Long,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<GroupDetailsModel>
 
     @GET("api/group/list-user-groups")
     suspend fun listUserGroups(
-        @Query("userId") userId: Long
+        @Query("userId") userId: Long,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<List<GroupsModel>>
 
     @GET("api/group/search-member")
     suspend fun searchMember(
         @Query("groupId") groupId: Long,
         @Query("search") search: String,
-        @Query("userId") userId: Long
+        @Query("userId") userId: Long,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<SearchResponseModel>
 
     @POST("api/group/update-group")
     suspend fun updateGroup(
-        @Body groupSaveModel: GroupSaveModel
+        @Body groupSaveModel: GroupSaveModel,
+        @Header("Authorization") bearerToken: String = Constants.BEARER_TOKEN
     ): Response<ResponseModel>
 
 }
