67175a18881b0f59958c89e858e118f923489ba0
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-12-04 16:36:14 +0100

    UserSearchScreen: generate basic layout

---
+++ app/src/main/java/com/example/eventwise/screens/usersearch/UserSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eventwise.screens.usersearch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eventwise.R
+
+class UserSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_search)
+    }
+}
