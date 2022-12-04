ac24143e32de429c6fe01bc746eabeb617b9db84
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-12-04 12:26:31 +0100

    CreateEventScreen: handle errors

--- app/src/main/java/com/example/eventwise/screens/createevent/CreateEventRepository.kt
+++ app/src/main/java/com/example/eventwise/screens/createevent/CreateEventRepository.kt
@@ -1,11 +1,24 @@
 package com.example.eventwise.screens.createevent
 
+import androidx.lifecycle.MutableLiveData
 import com.example.eventwise.models.EventSaveModel
 import com.example.eventwise.services.GatewayApi
 
 class CreateEventRepository {
 
-    suspend fun createEvent(eventSaveModel: EventSaveModel) {
-        GatewayApi.gatewayService.createEvent(eventSaveModel)
+    suspend fun createEvent(
+        success: MutableLiveData<Boolean>,
+        errorMessage: MutableLiveData<String>,
+        eventSaveModel: EventSaveModel
+    ) {
+        val request = GatewayApi.gatewayService.createEvent(eventSaveModel)
+
+        if (request.code() !in 200..299){
+            errorMessage.value = request.errorBody().toString()
+            success.value = false
+        } else {
+            success.value = true
+            errorMessage.value = null
+        }
     }
 }
